0b1c5811c87559e0be642907be095d704c21914c
c7293693 <m.tomlinson8562@student.leedsbeckett.ac.uk>
2024-01-22 12:19:10 +0000

    Added unit test for existing CommandFactory class

---
+++ ProgrammingLanguageGUITest/commands/CommandFactoryTest.cs
@@ -0,0 +1,33 @@
+using ProgrammingLanguageGUI.commands;
+using ProgrammingLanguageGUI.exception;
+
+namespace ProgrammingLanguageGUITest {
+    /// <summary>
+    /// Tests relating to the CommandFactory factory class.
+    /// </summary>
+    [TestClass]
+    public class CommandFactoryTest {
+
+        /// <summary>
+        /// Tests that the BuildCommand method returns a Command with the correct
+        /// derived type with a valid command type.
+        /// </summary>
+        [TestMethod]
+        [DataRow("MOVE 100 100", typeof(Move))]
+        [DataRow("DRAWTO 100 100", typeof(DrawTo))]
+        [DataRow("CIRCLE 100", typeof(Circle))]
+        [DataRow("CLEAR", typeof(Clear))]
+        public void CommandFactoryShouldReturnCommandWithValidCommandType(string command, Type expectedType) {
+            Assert.IsInstanceOfType(CommandFactory.BuildCommand(command), expectedType);
+        }
+
+        /// <summary>
+        /// Tests that the BuildCommand method returns null with an invalid/not implemented
+        /// command type.
+        /// </summary>
+        [TestMethod]
+        public void CommandFactoryShouldReturnNullWithInvalidCommandType() {
+            Assert.IsNull(CommandFactory.BuildCommand("INVALID"));
+        }
+    }
+}
